7dcbf6cff80394227d91c08d18c2c46d5aa8e125
Arthur <ah98lg@gmail.com>
2022-09-11 22:56:48 -0300

    Adding CORS and some authorization functions

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,48 @@
-import express from "express"
-import mongoose from "mongoose"
-import routes from "./shared/routes/https/routes"
+import mongoose from "mongoose";
+import routes from "./shared/routes/https/routes";
+import cors from "cors";
+import express from "express";
 
-require('dotenv').config()
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(express.urlencoded({
-    extended:true
-}))
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
-app.use(routes)
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@monitoringapicluster.0vcy4nd.mongodb.net/?retryWrites=true&w=majority`)
-.then(() => {
-    
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  next();
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@monitoringapicluster.0vcy4nd.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.use(express.json());
+    app.use(routes);
+    app.use(cors());
     app.listen(parseInt(process.env.PORT || "3333"), () => {
-        console.log(`Server started on port ${parseInt(process.env.PORT || "3333")}`);
-      });
-    
-
-})
-.catch((error) => {
-    console.log(error)
-})
-
+      console.log(
+        `Server started on port ${parseInt(process.env.PORT || "3333")}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
