9977507013c1c63450200a89a9956d7cdfc3f64f
Arthur <ah98lg@gmail.com>
2022-09-16 00:44:16 -0300

    Cors error

--- src/server.ts
+++ src/server.ts
@@ -15,21 +15,7 @@ app.use(
 
 app.use("/uploads", express.static("./uploads"));
 
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  next();
-});
+app.use(cors({ origin: "http://myappurl.com", credentials: true }));
 
 mongoose
   .connect(
