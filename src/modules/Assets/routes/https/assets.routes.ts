8a759e9fb690ab64bf20ca94b959ac21aa57dbdb
Arthur <ah98lg@gmail.com>
2022-09-15 23:59:38 -0300

    Deploy

--- src/modules/Assets/routes/https/assets.routes.ts
+++ src/modules/Assets/routes/https/assets.routes.ts
@@ -1,14 +1,9 @@
 import { Router } from "express";
-import { upload } from "../../../../../configs/uploads";
 import assetsController from "../../controller/AssetsController";
 
 const assetsRouter = Router();
 
-assetsRouter.post(
-  "/:company_id/:unity_id",
-  upload,
-  assetsController.registerNewAsset
-);
+assetsRouter.post("/:company_id/:unity_id", assetsController.registerNewAsset);
 assetsRouter.delete(
   "/:company_id/:unity_id/:asset_id",
   assetsController.deleteAsset
